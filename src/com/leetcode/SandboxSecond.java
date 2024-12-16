6a7c0461c5e5a1aae763c5e433023229ef8ea78e
C5329198 <maryia.sinkouskaya@sap.com>
2024-12-16 13:50:34 +0300

    34 task

--- src/com/leetcode/SandboxSecond.java
+++ src/com/leetcode/SandboxSecond.java
@@ -3,8 +3,31 @@ package com.leetcode;
 public class SandboxSecond {
     public static void main(String[] args) {
         //task 33
-        ListNode list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-        System.out.println(deleteMiddle(list));
+//        ListNode list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+//        System.out.println(deleteMiddle(list));
+
+        //task34
+        System.out.println(getFinalState(new int[] {2,1,3,5,6}, 5,2));
+    }
+
+    public static int[] getFinalState(int[] nums, int k, int multiplier) {
+        for (int i = 0; i < k; i++) {
+            int index = getMinIndex(nums);
+            nums[index] *= multiplier;
+        }
+        return nums;
+    }
+
+    private static int getMinIndex(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < min) {
+                index = i;
+                min = nums[i];
+            }
+        }
+        return index;
     }
 
     public static ListNode deleteMiddle(ListNode head) {
